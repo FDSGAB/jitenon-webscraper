ab580a37238958a57943fa28c2255650380f9e1e
Gabriel Fioravante Di Sciascio <69602642+FDSGAB@users.noreply.github.com>
2024-05-17 01:48:05 -0300

    Deleted note and test files

--- link_validator.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-try:
-    page = requests.get('https://kanji.jitenon.jp/kanji/501.html')
-    if page.status_code != 200:
-        print("Warning")
-except:
-    input("Error found at:")
